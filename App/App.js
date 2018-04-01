2033f4bc9fe5dab7c8d82a3f0d356f230ca85c61
Kirsi Jaasalmi <kirsi.jaasalmi@iki.fi>
2018-04-01 10:25:31 +0300

    Some content added to frontend

--- App/App.js
+++ App/App.js
@@ -1,13 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { Component, Math } from 'react';
+import { StyleSheet, AppRegistry, Text, View, Button } from 'react-native';
+
+var time = 0; 
+
+function tick() {
+  time++;
+  /*
+  var seconds = Math.floor(time%60); 
+  var minutes = Math.floor((time/60)%60);
+  var hours = Math.floor(time/(60*60)%24);
+  const element = (
+    <div>
+      {hours}:{minutes}.{seconds}
+    </div>
+  );
+*/
+  return 'element';
+}
+
+
+class Tick extends Component {
+  constructor(props) {
+    super(props);
+    //this.state = {isCounting: true};
+    //this.time++;
+  }
+  render() {
+    let display = tick();
+    return (
+         
+      <Text>'{display}'</Text>
+    );
+  }
+  /*
+  startTimer(){
+    // Set timer state according to previous state
+    this.setState( isCounting => !(isCounting) );
+  }*/
+}
+// Toggle the state every second
+setInterval(tick, 1000);
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.grid}>
+            <Text>Timesheet</Text>
+              <View style={styles.row}>
+                  <Button
+                    onPress={tick}
+                    title="Start"
+                    color="red"
+                    accessibilityLabel="Start"
+                  />
+                  <Tick></Tick>
+              </View>
+        </View>
+        <View style={styles.grid}>
+            <Text>Listausta</Text>
+              <View style={styles.row}>
+                  <Text>Tähän jotain</Text>
+              </View>
+        </View> 
       </View>
     );
   }
@@ -16,8 +72,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
+  },
+  grid: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
   },
 });
