93ddcbfc9f3992f39b70cd62cec8aad3603ec668
Kirsi Jaasalmi <kirsi.jaasalmi@iki.fi>
2018-04-08 22:03:03 +0300

    Some timer code added
    Some timer code added

--- App/App.js
+++ App/App.js
@@ -1,69 +1,100 @@
 
-import React, { Component, Math } from 'react';
-import { StyleSheet, AppRegistry, Text, View, Button } from 'react-native';
-
-var time = 0; 
-
-function tick() {
-  time++;
-  /*
-  var seconds = Math.floor(time%60); 
-  var minutes = Math.floor((time/60)%60);
-  var hours = Math.floor(time/(60*60)%24);
-  const element = (
-    <div>
-      {hours}:{minutes}.{seconds}
-    </div>
-  );
-*/
-  return 'element';
-}
-
-
-class Tick extends Component {
-  constructor(props) {
-    super(props);
-    //this.state = {isCounting: true};
-    //this.time++;
-  }
-  render() {
-    let display = tick();
-    return (
-         
-      <Text>'{display}'</Text>
-    );
-  }
-  /*
-  startTimer(){
-    // Set timer state according to previous state
-    this.setState( isCounting => !(isCounting) );
-  }*/
-}
-// Toggle the state every second
-setInterval(tick, 1000);
+import React, { Math, Component } from 'react';
+import { StyleSheet, AppRegistry, Text, View, Button, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+			time: 1,
+			intervalId: false,
+			seconds: 0,
+			minutes: 0,
+      hours: 0,
+      project: '',
+      currenttime: this.currentTime(),
+      currentdate: this.currentDate()
+	  }
+	  this.start = this.start.bind(this)
+		this.stop = this.stop.bind(this)
+	}
+
+	start() {
+    if( !this.state.intervalId ){
+		var intervalId = setInterval(() => {
+		  this.setState({ 
+			intervalId: intervalId, 
+      time: this.state.time + 1,
+      currenttime: 'this.currentTime()',
+      currentdate: 'this.currentDate()' })
+			this.tick();
+			}, 10)
+	  }    
+	}
+
+  currentDate(){
+    return new Date().toLocaleDateString('fi-FI', { timeZone: 'UTC'});
+  }
+
+  currentTime(){
+    return new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+  }
+
+	stop() {
+	  if( this.state.intervalId ){
+      clearInterval(this.state.intervalId);
+      this.setState({ 
+        time: 0, 
+        intervalId: false })
+      }
+
+      // Save time to DB
+	}
+
+	tick () {
+      var time = this.state.time;
+      if( time > 0){
+        var seconds = Math.floor(time%60); 
+        var minutes = Math.floor((time/60)%60);
+        var hours = Math.floor(time/(60*60)%24);
+        this.setState({
+          seconds: seconds,
+          minutes: minutes,
+          hours: hours})
+      }      
+	}
+
+  
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.grid}>
-            <Text>Timesheet</Text>
+            
+            <TextInput
+                style={{height: 40}}
+                placeholder="Give project name"
+                onChangeText={(project) => this.setState({project})}
+              />
+               
               <View style={styles.row}>
                   <Button
-                    onPress={tick}
+                    onPress={this.start}
                     title="Start"
-                    color="red"
+                    color="green"
                     accessibilityLabel="Start"
                   />
-                  <Tick></Tick>
+                  <Button
+                    onPress={this.stop}
+                    title="Stop"
+                    color="red"
+                    accessibilityLabel="Stop"
+                  />
+                 <Text>
+                  {this.state.currentdate} {this.state.currenttime} - {this.state.hours}:{this.state.minutes}:{this.state.seconds} 
+                  {this.state.project}
+                </Text>
               </View>
         </View>
-        <View style={styles.grid}>
-            <Text>Listausta</Text>
-              <View style={styles.row}>
-                  <Text>Tähän jotain</Text>
-              </View>
-        </View> 
       </View>
     );
   }
