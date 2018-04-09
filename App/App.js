898d46ef5c52dc240fbb1a4d2aee17f0c7ecd830
Kirsi Jaasalmi <kirsi.jaasalmi@iki.fi>
2018-04-09 21:44:21 +0300

    Timer fixed
    Timer fixed

--- App/App.js
+++ App/App.js
@@ -1,6 +1,6 @@
 
-import React, { Math, Component } from 'react';
-import { StyleSheet, AppRegistry, Text, View, Button, TextInput } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, AppRegistry, Text, View, Button, TextInput, List, FlatList } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -13,7 +13,8 @@ export default class App extends React.Component {
       hours: 0,
       project: '',
       currenttime: this.currentTime(),
-      currentdate: this.currentDate()
+      currentdate: this.currentDate(),
+      starttime: false,
 	  }
 	  this.start = this.start.bind(this)
 		this.stop = this.stop.bind(this)
@@ -23,32 +24,49 @@ export default class App extends React.Component {
     if( !this.state.intervalId ){
 		var intervalId = setInterval(() => {
 		  this.setState({ 
-			intervalId: intervalId, 
-      time: this.state.time + 1,
-      currenttime: 'this.currentTime()',
-      currentdate: 'this.currentDate()' })
-			this.tick();
-			}, 10)
+        starttime: new Date(),
+        intervalId: intervalId, 
+        time: this.state.time + 1,
+        currenttime: this.currentTime(),
+        currentdate: this.currentDate()})
+        this.tick();
+        }, 1000)
 	  }    
 	}
 
   currentDate(){
-    return new Date().toLocaleDateString('fi-FI', { timeZone: 'UTC'});
+    var date =  new Date();
+    var formattedDate = date.getDate()+'.'+(date.getMonth()+1) +'.'+date.getFullYear()
+    return formattedDate
   }
 
   currentTime(){
-    return new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+    var date =  new Date();
+    var minutes = (date.getMinutes() < 10) ? '0'+date.getMinutes():date.getMinutes()
+    var hours = (date.getHours() < 10) ? '0'+date.getHours():date.getHours()
+    var formattedTime = hours+':'+minutes
+    return formattedTime;
   }
 
 	stop() {
 	  if( this.state.intervalId ){
+      var projectdata = {
+        'name': this.state.project,
+        'time': this.state.time,
+        'starttime': this.state.starttime,
+      }
+        
       clearInterval(this.state.intervalId);
       this.setState({ 
         time: 0, 
+        seconds: 0,
+        minutes: 0,
+        hours: 0,
+        project: '',
         intervalId: false })
       }
 
-      // Save time to DB
+      // Save project data to DB
 	}
 
 	tick () {
@@ -68,32 +86,32 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.grid}>
-            
-            <TextInput
+        <View>
+          <View>
+          <TextInput
                 style={{height: 40}}
                 placeholder="Give project name"
                 onChangeText={(project) => this.setState({project})}
               />
-               
-              <View style={styles.row}>
-                  <Button
-                    onPress={this.start}
-                    title="Start"
-                    color="green"
-                    accessibilityLabel="Start"
-                  />
-                  <Button
-                    onPress={this.stop}
-                    title="Stop"
-                    color="red"
-                    accessibilityLabel="Stop"
-                  />
-                 <Text>
-                  {this.state.currentdate} {this.state.currenttime} - {this.state.hours}:{this.state.minutes}:{this.state.seconds} 
-                  {this.state.project}
-                </Text>
-              </View>
+              <Button
+                onPress={this.start}
+                title="Start"
+                color="green"
+                accessibilityLabel="Start"
+              />
+              <Button
+                onPress={this.stop}
+                title="Stop"
+                color="red"
+                accessibilityLabel="Stop"
+              />
+              
+            </View>
+            <View>    
+                <Text>
+                {this.state.currentdate} {this.state.currenttime} - {this.state.hours}:{this.state.minutes}:{this.state.seconds}  {this.state.project}
+              </Text>
+            </View>
         </View>
       </View>
     );
@@ -110,6 +128,11 @@ const styles = StyleSheet.create({
   grid: {
     flex: 1,
     flexDirection: 'column',
+  },
+  column: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
   },
   row: {
     flex: 1,
