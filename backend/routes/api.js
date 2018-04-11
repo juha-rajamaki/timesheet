0b17890f0acc2d0123eb72422b7fe30aadd0b7f8
KariNarhinen <kari@inia.fi>
2018-04-11 17:10:59 +0300

    added project and user paths to api

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -4,13 +4,18 @@ var router = express.Router();
 
 // Models
 var TimesModel = require('../models/times');
-
+var UsersModel = require('../models/users');
+var ProjectsModel = require('../models/projects');
 // Routes
 //router.get('/timeaction', function(req,res){
 //    res.send('API responded')
 //});
 TimesModel.methods(['get', 'put', 'post', 'delete']);
 TimesModel.register(router, '/timesheet');
+UsersModel.methods(['get', 'put', 'post', 'delete']);
+UsersModel.register(router, '/user');
+ProjectsModel.methods(['get', 'put', 'post', 'delete']);
+ProjectsModel.register(router, '/project');
 
 
 // Return Router
